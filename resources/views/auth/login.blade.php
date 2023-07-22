d35f990bb96703b8ad1a28cf19fb3d8d6e6b28ce
andreyshirokoff <104889650+andreyshirokoff@users.noreply.github.com>
2023-07-23 03:51:22 +0600

    back-login-theme

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('css/themes/myClient.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/themes/beautyCheck.css')}}">
     <link rel="stylesheet" href="{{asset('css/login.css')}}">

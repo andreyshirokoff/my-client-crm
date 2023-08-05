c03f4ae16e8ab690318b97a2ad391e967d3ce0c1
andreyshirokoff <104889650+andreyshirokoff@users.noreply.github.com>
2023-08-05 16:33:13 +0600

    ready-tariff-api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use App\Models\Packet;
 use App\Models\Tariff;
 use App\Models\User;
 use App\Models\UserGroup;
+use App\Models\UserTariff;
 use Bitrix\Translate\Controller\Asset\Pack;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -62,19 +63,126 @@ class ApiController extends Controller
 
                     break;
 
-                case 'packet.list':
-                    $packets = Packet::all();
+                case 'userTariff.list':
+                    $userTariffs = Tariff::all();
 
-                    if($packets !== false)
+                    if($userTariffs !== false)
                     {
-                        $statusRequest = $this->statusRequest('OK', 'The packets are successfully returned', $packets->toArray());
+                        $statusRequest = $this->statusRequest('OK', 'The user_tariffs are successfully returned', $userTariffs->toArray());
                     }
                     else
                     {
                         $statusRequest = $this->statusRequest('ERROR', 'This table is not available');
                     }
 
+
                     break;
+
+                case 'tariff.get':
+                    $id = $request->input('ID');
+
+                    if($id)
+                    {
+                        $tariff = Tariff::where('id', $id)->first()->toArray();
+
+                        if(count($tariff) > 0)
+                        {
+                            $statusRequest = $this->statusRequest('OK', 'The tariff is successfully returned', $tariff);
+                        }
+                        else
+                        {
+                            $statusRequest = $this->statusRequest('ERROR', 'This tariff does not exist');
+                        }
+                    }
+                    else
+                    {
+                        $statusRequest = $this->statusRequest('ERROR', 'The ID field is empty');
+                    }
+
+                    break;
+
+                case 'userTariff.get':
+                    $id = $request->input('ID');
+
+                    if($id)
+                    {
+                        $tariff = UserTariff::where('id', $id)->first()->toArray();
+
+                        if(count($tariff) > 0)
+                        {
+                            $statusRequest = $this->statusRequest('OK', 'The user_tariff is successfully returned', $tariff);
+                        }
+                        else
+                        {
+                            $statusRequest = $this->statusRequest('ERROR', 'This user_tariff does not exist');
+                        }
+                    }
+                    else
+                    {
+                        $statusRequest = $this->statusRequest('ERROR', 'The ID field is empty');
+                    }
+
+                    break;
+
+                case 'tariff.firstOrNew':
+                    $fields = json_decode($request->input('fields'), 1);
+                    if($fields)
+                    {
+                        $tariffCreate = Tariff::firstOrNew($fields);
+                        $tariffCreate->save();
+                        if($tariffCreate->id)
+                        {
+                            $statusRequest = $this->statusRequest('OK', 'The tariff is successfully added or updated', ['ID' => $tariffCreate->id]);
+                        }
+                        else
+                        {
+                            $statusRequest = $this->statusRequest('ERROR', 'Check the fields');
+                        }
+                    }
+                    else
+                    {
+                        $statusRequest = $this->statusRequest('ERROR', 'The "fields" field is empty');
+                    }
+
+                    break;
+
+                case 'userTariff.firstOrNew':
+                    $fields = json_decode($request->input('fields'), 1);
+                    if($fields)
+                    {
+                        $userTariffCreate = UserTariff::firstOrNew($fields);
+                        $userTariffCreate->save();
+                        if($userTariffCreate->id)
+                        {
+                            $statusRequest = $this->statusRequest('OK', 'The user tariff is successfully added or updated', ['ID' => $userTariffCreate->id]);
+                        }
+                        else
+                        {
+                            $statusRequest = $this->statusRequest('ERROR', 'Check the fields');
+                        }
+                    }
+                    else
+                    {
+                        $statusRequest = $this->statusRequest('ERROR', 'The "fields" field is empty');
+                    }
+
+                    break;
+
+
+
+//                case 'packet.list':
+//                    $packets = Packet::all();
+//
+//                    if($packets !== false)
+//                    {
+//                        $statusRequest = $this->statusRequest('OK', 'The packets are successfully returned', $packets->toArray());
+//                    }
+//                    else
+//                    {
+//                        $statusRequest = $this->statusRequest('ERROR', 'This table is not available');
+//                    }
+//
+//                    break;
 
                 case 'user.get':
                     $id = $request->input('ID');
@@ -143,51 +251,30 @@ class ApiController extends Controller
 
                     break;
 
-                case 'tariff.get':
-                    $id = $request->input('ID');
 
-                    if($id)
-                    {
-                        $tariff = Tariff::where('id', $id)->first()->toArray();
 
-                        if(count($tariff) > 0)
-                        {
-                            $statusRequest = $this->statusRequest('OK', 'The tariff is successfully returned', $tariff);
-                        }
-                        else
-                        {
-                            $statusRequest = $this->statusRequest('ERROR', 'This tariff does not exist');
-                        }
-                    }
-                    else
-                    {
-                        $statusRequest = $this->statusRequest('ERROR', 'The ID field is empty');
-                    }
-
-                    break;
-
-                case 'packet.get':
-                    $id = $request->input('ID');
-
-                    if($id)
-                    {
-                        $packet = Packet::where('id', $id)->first()->toArray();
-
-                        if(count($packet) > 0)
-                        {
-                            $statusRequest = $this->statusRequest('OK', 'The packet is successfully returned', $packet);
-                        }
-                        else
-                        {
-                            $statusRequest = $this->statusRequest('ERROR', 'This packet does not exist');
-                        }
-                    }
-                    else
-                    {
-                        $statusRequest = $this->statusRequest('ERROR', 'The ID field is empty');
-                    }
-
-                    break;
+//                case 'packet.get':
+//                    $id = $request->input('ID');
+//
+//                    if($id)
+//                    {
+//                        $packet = Packet::where('id', $id)->first()->toArray();
+//
+//                        if(count($packet) > 0)
+//                        {
+//                            $statusRequest = $this->statusRequest('OK', 'The packet is successfully returned', $packet);
+//                        }
+//                        else
+//                        {
+//                            $statusRequest = $this->statusRequest('ERROR', 'This packet does not exist');
+//                        }
+//                    }
+//                    else
+//                    {
+//                        $statusRequest = $this->statusRequest('ERROR', 'The ID field is empty');
+//                    }
+//
+//                    break;
 
                 default:
                     $statusRequest = $this->statusRequest('ERROR', "You didn't pass the method or this method doesn't exits");

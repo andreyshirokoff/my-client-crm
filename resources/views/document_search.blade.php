47a2e9b4fed3b63bee9f778d85a905ead039aacb
andreyshirokoff <104889650+andreyshirokoff@users.noreply.github.com>
2023-08-10 23:54:51 +0600

    add-screambs-and-new-title

--- resources/views/document_search.blade.php
+++ resources/views/document_search.blade.php
@@ -1,14 +1,10 @@
 @extends('layout.app')
 
-{{--@section('page')--}}
-{{--    <a href="{{url('dashboard')}}">Strona główna</a>--}}
-{{--    <span style="font-weight: 500;">&nbsp;&gt;&nbsp;</span>--}}
-{{--    <a href="{{url('dashboard/client-search')}}">Znajdź klienta</a>--}}
-{{--    <span style="font-weight: 500;">&nbsp;&gt;&nbsp;</span>--}}
-{{--    <a href="{{url('dashboard/clients')}}?clientId={{$_GET['clientId']}}">Profil klienta</a>--}}
-{{--    <span style="font-weight: 500;">&nbsp;&gt;&nbsp;</span>--}}
-{{--    <a href="javascript:void(0)">Edytować zabieg</a>--}}
-{{--@endsection--}}
+@section('page')
+    <a href="{{url('dashboard')}}">Strona główna</a>
+    <span style="font-weight: 500;">&nbsp;&gt;&nbsp;</span>
+    <a href="javascript:void(0)">Baza klientów</a>
+@endsection
 
 @section('content')
 {{--    <div class="box">--}}
